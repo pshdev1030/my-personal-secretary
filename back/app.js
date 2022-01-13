3c0a8c359aa831ca6dd685f7e71c8f86d59098a0
ParkSungHyun <psh1030.dev@gmail.com>
2022-01-13 11:02:49 +0900

    Feat: express + mongoose setting
    add models/user.js to make Schema of user
    add /mongo.js to connect mongoose to app.js
    add router/user.js to make login and signup logic
    add /app.js is main file

---
+++ back/app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const { dbConnect } = require("./mongo");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors({ origin: "*", credentials: true }));
+const port = process.env.PORT || 7000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+dbConnect();
+
+const index = require("./router/index");
+app.use("/", index);
+
+const user = require("./router/user");
+app.use("/user", user);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Successfully connected to Server on port ${process.env.PORT}!`)
+);
