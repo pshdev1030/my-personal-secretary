2cd3b9e6cc644dcaf5c4e5556c711d1d2480be01
ParkSungHyun <psh1030.dev@gmail.com>
2022-01-17 00:36:03 +0900

    Fix: add auto login after signup
    remove hooks/useInput to use antd default form logic
    add auto login after signup to components/SignUpForm

--- front/components/SignupForm.tsx
+++ front/components/SignupForm.tsx
@@ -1,55 +1,54 @@
-import React, { ReactElement, useCallback } from "react";
+import React, { ReactElement, useCallback, useRef } from "react";
 import { Input, Form, Button } from "antd";
 import { toast } from "react-toastify";
-import useInput from "hooks/useInput";
 import axios from "axios";
-const SignUpForm = (): ReactElement => {
-  const [email, onChangeEmail, setEmail] = useInput("");
-  const [password, onChangePassword, setPassword] = useInput("");
-  const [username, onChangeUserame, setUsername] = useInput("");
+import { useSWRConfig } from "swr";
 
-  const onSubmit = useCallback(
-    async (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (
-        email.trim().length === 0 ||
-        password.trim().length === 0 ||
-        username.trim().length === 0
-      ) {
-        toast.error("모든 값을 입력해주세요");
-      }
-      try {
-        const result = await axios.post("http://localhost:8000/user", {
-          email,
-          username: username,
-          password,
-        });
-        setEmail("");
-        setUsername("");
-        setPassword("");
-      } catch (err: any) {
-        console.log(err.response);
-        if (err.response.data.message) {
-          toast.error(err.response.data.message);
-          setEmail("");
-          setPassword("");
-          return;
-        }
-        toast.error("에러가 발생하였습니다.");
-      }
-    },
-    [email, password, username]
-  );
+interface signUpRequestType {
+  signUpEmail: string;
+  signUpPassword: string;
+  username: string;
+}
+
+const SignUpForm = (): ReactElement => {
+  const { mutate } = useSWRConfig();
+  const onSubmit = useCallback(async (data: signUpRequestType) => {
+    const { signUpEmail: email, signUpPassword: password, username } = data;
+    if (
+      email.trim().length === 0 ||
+      password.trim().length === 0 ||
+      username.trim().length === 0
+    ) {
+      toast.error("모든 값을 입력해주세요");
+    }
+    const signUpRequest = axios
+      .post("http://localhost:8000/user", {
+        email,
+        username,
+        password,
+      })
+      .then((r) => mutate("http://localhost:8000/user/login", r.data));
+    toast.promise(signUpRequest, {
+      pending: "회원가입 중입니다.",
+      success: "회원가입에 성공하였습니다. 곧 메인페이지로 이동합니다.",
+      error: {
+        render({ data }: any) {
+          return data.response.data.message;
+        },
+      },
+    });
+  }, []);
 
   return (
     <Form onFinish={onSubmit}>
       <Form.Item label="email" name="signUpEmail">
-        <Input type="email" value={email} onChange={onChangeEmail} />
+        <Input type="email" />
       </Form.Item>
       <Form.Item label="username" name="username">
-        <Input type="text" value={username} onChange={onChangeUserame} />
+        <Input type="text" />
       </Form.Item>
       <Form.Item label="passowrd" name="signUpPassword">
-        <Input type="password" value={password} onChange={onChangePassword} />
+        <Input type="password" />
       </Form.Item>
       <Button type="primary" htmlType="submit">
         가입하기
