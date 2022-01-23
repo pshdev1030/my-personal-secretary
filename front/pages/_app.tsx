e2d0304a218a0d99f3f793b67191bc60e3add59c
ParkSungHyun <psh1030.dev@gmail.com>
2022-01-23 15:17:23 +0900

    Fix: add global style

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -5,12 +5,30 @@ import "@fullcalendar/common/main.css";
 import "@fullcalendar/daygrid/main.css";
 import "@fullcalendar/timegrid/main.css";
 import Head from "next/head";
+import { Global, css } from "@emotion/react";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>My Personal Secretary</title>
       </Head>
+      <Global
+        styles={css`
+          .ant-row {
+            margin-right: 0 !important;
+            margin-left: 0 !important;
+          }
+
+          .ant-col:first-child {
+            padding-left: 0 !important;
+          }
+
+          .ant-col:last-child {
+            padding-right: 0 !important;
+          }
+        `}
+      />
       <Component {...pageProps} />
     </>
   );
