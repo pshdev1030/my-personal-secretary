e9cb139333e6e1b38fcc1d6772565c1d41d83855
ParkSungHyun <psh1030.dev@gmail.com>
2022-01-24 14:50:00 +0900

    Fix: modify error
    if token is Bearer undefined return res.sendStatus(401)

--- back/modules/auth.js
+++ back/modules/auth.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
+  if (token == null || undefined) return res.sendStatus(401);
   jwt.verify(token, process.env.ACCESS_TOKEN, (err, userId) => {
     if (err) return res.sendStatus(403);
     req.body.userId = userId.id;
